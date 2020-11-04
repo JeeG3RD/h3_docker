b7cd1c872a1ef1323403c3028f90b950f147d50c
JeeG3RD <gerdy.jerome@gmail.com>
2020-11-04 11:21:16 +0100

    Add TP 4 (Heroku fix in progress)

---
+++ tp4_heroku/main.py
@@ -0,0 +1,17 @@
+import streamlit
+import joblib
+
+#Import du modèle entrainé
+model = joblib.load("model.pkl")
+
+#Récupération de la saisie utilisateur
+text_input = streamlit.text_input("Saisir un texte :", "")
+
+if(text_input):
+    #Calcul de la probabilité d'offensivité du texte
+    pred = model.predict_proba([text_input])[0]
+
+    streamlit.text("Resultat des estimations : ")
+    streamlit.text("Hate speech : " + str(round(pred[0]*100, 2)) + " %")
+    streamlit.text("Offensive language : " + str(round(pred[1]*100, 2)) + " %")
+    streamlit.text("Neither : " + str(round(pred[2]*100, 2)) + " %")
---
+++ tp4_heroku/readme.md
@@ -0,0 +1,27 @@
+# Installation de Heroku (Avec snap sous Manjaro)
+``` sudo snap install --classic heroku ```
+
+## Connecion à Heroku
+
+``` heroku login ```
+
+## Connexion au Container Registry
+``` heroku container:login ```
+
+## Push de l'application
+
+```heroku container:push web --app nom-de-l-app```
+
+## Publication de l'app
+
+```heroku container:release web --app nom-de-l-app```
+
+## Afficher les logs d'une application
+
+``` heroku logs --app nom-de-l-app ```
+
+
+### Adresse de l'application
+https://h3-docker-tp4-jg3rd.herokuapp.com/
+
+! Retourne une erreur actuellement
